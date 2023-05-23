7403fb61ab9d99a57a6ac74a89911640455b51ad
Arpit Agrawal <arpit23agrawal@gmail>
2023-05-23 08:50:03 +0530

    Adding A2

--- src/basicConcepts/StringPrac.java
+++ src/basicConcepts/StringPrac.java
@@ -4,6 +4,8 @@ public class StringPrac {
 
 	public static void main(String[] args) {
 		System.out.println("Updated new list");
-		System.out.println("Conflict Resolved");
+		System.out.println("Conflict Resolved - Again doining changes in A2 branch");
+		System.out.println("Adding new line line1 on A2 branch");
+		System.out.println("Adding new line line2 on A2 branch");
 		}
 }
