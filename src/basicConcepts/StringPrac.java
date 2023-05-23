02dd27a96b02d71359008b169456a69b1fc8a711
Arpit Agrawal <arpit23agrawal@gmail>
2023-05-23 08:55:00 +0530

    Merging branch B2 with conflict code

--- src/basicConcepts/StringPrac.java
+++ src/basicConcepts/StringPrac.java
@@ -4,6 +4,8 @@ public class StringPrac {
 
 	public static void main(String[] args) {
 		System.out.println("Updated new list");
-		System.out.println("Conflict Resolved");
+		System.out.println("Conflict Resolved---- updating line on Brach B2");
+		System.out.println("Adding line---- on Brach B2");
+		System.out.println("Adding line - line2---- on Brach B2");
 		}
 }
